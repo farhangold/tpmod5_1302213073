34490899c6b6ee19cb52f1b2300484391c8d5e98
Farhan Mulya Argyanto <mulyaargyanto@gmail.com>
2023-03-19 22:11:08 +0700

    menambahkan dataGeneric

--- tpmod5_1302213073/tpmod5_1302213073/Program.cs
+++ tpmod5_1302213073/tpmod5_1302213073/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataGeneric dataGeneric = new DataGeneric("1302213073");
+        dataGeneric.printData();
+    }
+}
+
+internal class DataGeneric
+{
+    private string v;
+
+    public DataGeneric(string v)
+    {
+        this.v = v;
+    }
+
+    internal void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah :"+v);
+    }
+}
