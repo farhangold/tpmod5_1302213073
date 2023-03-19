3946afd518bfdd11bee585d13cb1c14c9596683c
Farhan Mulya Argyanto <mulyaargyanto@gmail.com>
2023-03-19 22:04:27 +0700

    menambahkan class HaloGeneric dengan method

--- tpmod5_1302213073/tpmod5_1302213073/Program.cs
+++ tpmod5_1302213073/tpmod5_1302213073/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HelloGeneric helloGeneric = new HelloGeneric;
+        helloGeneric.SapaUser("Farhan Mulya Argaynto");
+    }
+}
+
+internal class HelloGeneric
+{
+    public void SapaUser(string username)
+    {
+        Console.WriteLine("Hallo User "+ username);
+    }
+}
